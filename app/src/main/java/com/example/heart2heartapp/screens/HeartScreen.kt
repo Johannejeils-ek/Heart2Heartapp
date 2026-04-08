b99dfb1e76e98ca150354d0dabcf5fd5634176d4
johannejeils-ek <joje@stud.ek.dk>
2026-04-08 11:35:48 +0200

    HeartScreen.kt Update

--- app/src/main/java/com/example/heart2heartapp/screens/HeartScreen.kt
+++ app/src/main/java/com/example/heart2heartapp/screens/HeartScreen.kt
@@ -1,10 +1,15 @@
 package com.example.heart2heartapp.screens
 
+import androidx.compose.foundation.Image
+import com.example.heart2heartapp.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
@@ -12,8 +17,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.heart2heartapp.ui.theme.BricolageFont
+import com.example.heart2heartapp.ui.theme.PoppinsFont
+
 
 @Composable
 fun HeartScreen(name: String, onArgumentsButtonClick: () -> Unit) {
@@ -29,35 +40,56 @@ fun HeartScreen(name: String, onArgumentsButtonClick: () -> Unit) {
     ) {
         Text(
             text = "Din Heart2Heart",
-            fontSize = 50.sp
-
+            fontSize = 45.sp,
+            fontFamily = BricolageFont
         )
         Text(
             text = "nøglering er",
-            fontSize = 50.sp
+            fontSize = 45.sp,
+            fontFamily = BricolageFont
         )
         Text(
             text = "connected",
-            fontSize = 50.sp
+            fontSize = 45.sp,
+            fontFamily = BricolageFont
         )
+
 
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .weight(1f),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Bottom
+            verticalArrangement = Arrangement.Center
 
-            ) {
-            Button(onClick = onArgumentsButtonClick,
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = Color(0xFFFF77B7), // background color
-                    contentColor = Color.White          // text/icon color
-                )) {
-                Text(
-                    text = "Disconnect",
-                    fontSize = 24.sp
-                )
-            }
+        ) {
+            Image(
+                painter = painterResource(R.drawable.heartscreen),
+                contentDescription = "Heart screen image",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp),
+                contentScale = ContentScale.Fit  // scales image to fit without cropping
+            )
+        }
+
+
+
+        Button(
+            onClick = onArgumentsButtonClick,
+            modifier = Modifier
+                .padding(bottom = 40.dp)
+                .width(170.dp)
+                .height(55.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFFFF77B7),
+                contentColor = Color.White
+            )
+        ) {
+            Text(
+                text = "Disconnect",
+                fontSize = 16.sp,
+                fontFamily = PoppinsFont
+            )
         }
 
 
