ead1ba32a8942ca469f9343a9bc40d8f8b74ccc0
mathildefarnoe <mat_farnoe@hotmail.com>
2026-04-08 10:52:30 +0200

    fonts

---
+++ app/src/main/java/com/example/heart2heartapp/ui/theme/Fonts.kt
@@ -0,0 +1,14 @@
+package com.example.heart2heartapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.heart2heartapp.R
+
+val BricolageFont = FontFamily(
+    Font(R.font.bricolagegrotesque_medium, FontWeight.Medium)
+)
+
+val PoppinsFont = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal)
+)
