cba4bb96632f2fc473f07e8e2ce158f0a0f92209
johannejeils-ek <joje@stud.ek.dk>
2026-04-09 10:23:28 +0200

    Homescreen

--- app/src/main/java/com/example/heart2heartapp/classes/EventViewModel.kt
+++ app/src/main/java/com/example/heart2heartapp/classes/EventViewModel.kt
@@ -9,7 +9,18 @@ class EventViewModel: ViewModel() {
         Event("Stratego turnering på Bastard Café", 2),
         Event("Gratis BMO på Lagkagehuset", 3),
         Event("Lysshow på Bryggen", 4),
-        Event("Foredrag om Leverpostejsbogen af Münster", 5)
+        Event("Foredrag om Leverpostejsbogen af Münster", 5),
+        Event("Vinylmarked på Refshaleøen", 6),
+        Event("Brætspilsaften på Café Mellemrummet", 7),
+        Event("Silent disco i Kongens Have", 8),
+        Event("Street food festival på Papirøen", 9),
+        Event("Morgendans ved Ofelia Plads", 10),
+        Event("Quiznight på en brun bodega", 11),
+        Event("Krea-workshop: Lav din egen keramik", 12),
+        Event("Filmvisning under åben himmel i Fælledparken", 13),
+        Event("Ølsmagning på Nørrebro Bryghus", 14),
+        Event("Yoga på taget af Illum", 15)
+
     )
     val events: List<Event> = exampleEvents
 
