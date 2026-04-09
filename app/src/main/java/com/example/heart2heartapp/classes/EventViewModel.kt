5b1e30f64fc458a1e4ad6a2893708bfb85ed2dd4
Simon <junkersje@gmail.com>
2026-04-09 10:41:46 +0200

    Event and homepage

--- app/src/main/java/com/example/heart2heartapp/classes/EventViewModel.kt
+++ app/src/main/java/com/example/heart2heartapp/classes/EventViewModel.kt
@@ -6,21 +6,21 @@ import com.example.heart2heartapp.R
 class EventViewModel: ViewModel() {
 
     val exampleEvents: List<Event> = listOf(
-        Event("Sauna gus med Andreas Bo", 1),
-        Event("Stratego turnering på Bastard Café", 2),
-        Event("Gratis BMO på Lagkagehuset", 3),
-        Event("Lysshow på Bryggen", 4),
-        Event("Foredrag om Leverpostejsbogen af Münster", 5),
-        Event("Vinylmarked på Refshaleøen", 6),
-        Event("Brætspilsaften på Café Mellemrummet", 7),
-        Event("Silent disco i Kongens Have", 8),
-        Event("Street food festival på Papirøen", 9),
-        Event("Morgendans ved Ofelia Plads", 10),
-        Event("Quiznight på en brun bodega", 11),
-        Event("Krea-workshop: Lav din egen keramik", 12),
-        Event("Filmvisning under åben himmel i Fælledparken", 13),
-        Event("Ølsmagning på Nørrebro Bryghus", 14),
-        Event("Yoga på taget af Illum", 15)
+        Event("Sauna gus med Andreas Bo", 1, R.drawable.eventest),
+        Event("Stratego turnering på Bastard Café", 2, R.drawable.eventest),
+        Event("Gratis BMO på Lagkagehuset", 3, R.drawable.eventest),
+        Event("Lysshow på Bryggen", 4, R.drawable.eventest),
+        Event("Foredrag om Leverpostejsbogen af Münster", 5, R.drawable.eventest),
+        Event("Vinylmarked på Refshaleøen", 6, R.drawable.eventest),
+        Event("Brætspilsaften på Café Mellemrummet", 7, R.drawable.eventest),
+        Event("Silent disco i Kongens Have", 8, R.drawable.eventest),
+        Event("Street food festival på Papirøen", 9, R.drawable.eventest),
+        Event("Morgendans ved Ofelia Plads", 10, R.drawable.eventest),
+        Event("Quiznight på en brun bodega", 11, R.drawable.eventest),
+        Event("Krea-workshop: Lav din egen keramik", 12, R.drawable.eventest),
+        Event("Filmvisning under åben himmel i Fælledparken", 13, R.drawable.eventest),
+        Event("Ølsmagning på Nørrebro Bryghus", 14, R.drawable.eventest),
+        Event("Yoga på taget af Illum", 15, R.drawable.eventest)
 
     )
     val events: List<Event> = exampleEvents
